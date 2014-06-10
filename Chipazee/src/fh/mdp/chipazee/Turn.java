95d536c8a8e4cca734c5f68ce2e9e8ebc61b194e
sladiri <sladjanri@live.com>
2014-06-10 17:24:48 +0200

    Possibly reverted to previous (only?) way to serialize Turn. Added Serializable interface to Turn.

--- Chipazee/src/fh/mdp/chipazee/Turn.java
+++ Chipazee/src/fh/mdp/chipazee/Turn.java
@@ -7,16 +7,15 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OptionalDataException;
+import java.io.Serializable;
 import java.io.StreamCorruptedException;
 import java.nio.ByteBuffer;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class Turn {
+public class Turn implements Serializable {
 
 	public int turnNumber = 1;
-
-	private Deque<Colors> colors = new ArrayDeque<Colors>();
 
 	public void addColor(Colors color) {
 		colors.add(color);
@@ -27,23 +26,17 @@ public class Turn {
 	}
 
 	public static Turn deserialize(byte[] data) throws IllegalArgumentException {
-		ByteBuffer buffer = ByteBuffer.allocate(data.length);
-		InputStream stream = new ByteArrayInputStream(buffer.array(), 0,
-				buffer.limit());
+		InputStream stream = new ByteArrayInputStream(data, 0, data.length);
 		try {
 			ObjectInputStream ois = new ObjectInputStream(stream);
 			return (Turn) ois.readObject();
 		} catch (StreamCorruptedException e) {
-			e.printStackTrace();
 			throw new IllegalArgumentException(e);
 		} catch (OptionalDataException e) {
-			e.printStackTrace();
 			throw new IllegalArgumentException(e);
 		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
 			throw new IllegalArgumentException(e);
 		} catch (IOException e) {
-			e.printStackTrace();
 			throw new IllegalArgumentException(e);
 		}
 	}
@@ -56,10 +49,14 @@ public class Turn {
 			oos.flush();
 			return bos.toByteArray();
 		} catch (IOException e) {
-			e.printStackTrace();
 			throw e;
 		}
-		
 	}
+
+	private Deque<Colors> colors = new ArrayDeque<Colors>();
+	/**
+	 * Automatically added by Eclipse.
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
